854b9c248bbd6ac4a36bfb1b1a3a783465768a8f
s5064284 <34233747+s5064284@users.noreply.github.com>
2021-04-26 14:55:53 +0100

    Main method added to run Ssh.py

--- app/Ssh.py
+++ app/Ssh.py
@@ -5,79 +5,80 @@ from app.models import Environment, Server, Command
 import time, threading
 from datetime import datetime
 
+# Create thread
+# Open Connection for Ip Address
+# Run all commands for that server
+# Compare all outputs and qrite to DB
+# Kill thread
+# Wait for required time
+# Repeat
 
-class Ssh:
+def execute_commands(environment, server, commands):
+    # servers = self.get_servers(env_id)
+    # timing = self.get_timing(env_id)
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    key_file = os.path.expanduser('~/.ssh/id_rsa')
+    privatekeyfile = paramiko.RSAKey.from_private_key_file(key_file)
+    ssh.connect(hostname=server.ip_address, username=server.username, pkey=privatekeyfile)
+    print('Connected ' + server.ip_address)
+    print(datetime.now())
+    for command in commands:
+        stdin, stdout, stderr = ssh.exec_command(command.command)
+        output = stdout.readlines()
+        for line in output:
+            final_line = line.strip('\n')
+            compare(command.id, command.expectation, final_line)
+        # threading.Timer(timing, self.execute_commands, args=(ip, username, env_id)).start()
 
-    # Create thread
-    # Open Connection for Ip Address
-    # Run all commands for that server
-    # Compare all outputs and qrite to DB
-    # Kill thread
-    # Wait for required time
-    # Repeat
+def get_servers(env_id):
+    servers = Server.query.filter_by(env_id_fk=env_id).all()
+    return servers
 
-    def execute_commands(self, environment, server, commands):
-        # servers = self.get_servers(env_id)
-        # timing = self.get_timing(env_id)
-        ssh = paramiko.SSHClient()
-        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        key_file = os.path.expanduser('~/.ssh/id_rsa')
-        privatekeyfile = paramiko.RSAKey.from_private_key_file(key_file)
-        ssh.connect(hostname=server.ip_address, username=server.username, pkey=privatekeyfile)
-        print('Connected ' + server.ip_address)
-        print(datetime.now())
-        for command in commands:
-            stdin, stdout, stderr = ssh.exec_command(command.command)
-            output = stdout.readlines()
-            for line in output:
-                final_line = line.strip('\n')
-                self.compare(command.id, command.expectation, final_line)
-            # threading.Timer(timing, self.execute_commands, args=(ip, username, env_id)).start()
+def get_timing(env_id):
+    timing_query = Environment.query.filter_by(id=env_id).first()
+    timing = int(timing_query.__dict__['timing'])
+    return timing
 
-    def get_servers(self, env_id):
-        servers = Server.query.filter_by(env_id_fk=env_id).all()
-        return servers
-
-    def get_timing(self, env_id):
-        timing_query = Environment.query.filter_by(id=env_id).first()
-        timing = int(timing_query.__dict__['timing'])
-        return timing
-
-    def multithread(self):
-        thread_list = list()
-        environments = Environment.query.all()
-        event = threading.Event
-        while True:
-            for environment in environments:
-                servers = self.get_servers(environment.id)
-                for server in servers:
-                    commands = Command.query.filter_by(server_id_fk=server.id).all()
-                    # time.sleep(int(environment.timing))
-                    # t = threading.Thread(target=self.execute_commands, args=(environment, server, commands))
-                    print(environment.name)
-                    print(environment.timing)
-                    t = threading.Timer(int(environment.timing), self.execute_commands, args=(environment, server, commands))
-                    # time.sleep(int(environment.timing))
-                    t.start()
-                    thread_list.append(t)
-                    # print("Thread for " + server.ip_address + " started")
-                    # print(t)
-            for index, thread in enumerate(thread_list):
-                # print(index)
-                # print(thread)
-                thread.join()
-            print("All threads Finished")
+def multithread():
+    thread_list = list()
+    environments = Environment.query.all()
+    event = threading.Event
+    while True:
+        for environment in environments:
+            servers = get_servers(environment.id)
+            for server in servers:
+                commands = Command.query.filter_by(server_id_fk=server.id).all()
+                # time.sleep(int(environment.timing))
+                # t = threading.Thread(target=self.execute_commands, args=(environment, server, commands))
+                print(environment.name)
+                print(environment.timing)
+                t = threading.Timer(int(environment.timing), execute_commands, args=(environment, server, commands))
+                # time.sleep(int(environment.timing))
+                t.start()
+                thread_list.append(t)
+                # print("Thread for " + server.ip_address + " started")
+                # print(t)
+        for index, thread in enumerate(thread_list):
+            # print(index)
+            # print(thread)
+            thread.join()
+        print("All threads Finished")
 
 
-    def compare(self, command_id, expectation, live_output):
-        command = Command.query.filter_by(id=command_id).first()
-        if expectation == live_output:
-            command.command_status = 2
-            db.session.commit()
-        else:
-            command.command_status = 0
-            db.session.commit()
+def compare(command_id, expectation, live_output):
+    command = Command.query.filter_by(id=command_id).first()
+    if expectation == live_output:
+        command.command_status = 2
+        db.session.commit()
+    else:
+        command.command_status = 0
+        db.session.commit()
 
 
-ssh = Ssh()
-ssh.multithread()
+def main():
+    multithread()
+
+
+if __name__ == "__main__":
+    main()
