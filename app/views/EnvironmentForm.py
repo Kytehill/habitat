5ba8eb4d5e7740e1dae7632790d540591d068eaf
s5064284 <34233747+s5064284@users.noreply.github.com>
2021-05-08 14:22:39 +0100

    Adding validation

--- app/views/EnvironmentForm.py
+++ app/views/EnvironmentForm.py
@@ -5,6 +5,6 @@ from wtforms.validators import DataRequired, NumberRange
 
 class EnvironmentForm(FlaskForm):
     name = StringField('Name', validators=[DataRequired()])
-    timing = IntegerField('Timing', validators=[DataRequired(message='Please proved a number between 0 and 120'),
-                                                NumberRange(0, 120, message='Please proved a number between 0 and 120')])
+    timing = IntegerField('Timing', validators=[DataRequired(message='Please proved a time interval between 0 and 120 '
+                                                                     'seconds'), NumberRange(0, 120, message='Please provide a time interval between 0 and 120 seconds')])
     submit = SubmitField('Add Environment')
--- config.py
+++ config.py
@@ -3,7 +3,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'secure_key_required'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'random_key'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
                               'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
